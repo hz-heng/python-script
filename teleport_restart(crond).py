200e409ac50a3e26dee5b6d1af37b17e049f3418
hz-heng <hz_heng@qq.com>
2017-09-30 10:43:49 +0800

    Create teleport_restart(crond).py

---
+++ teleport_restart(crond).py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+#-*- coding:utf8 -*-
+
+import os, sys, logging
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='%(asctime)s %(levelname)s %(message)s',
+    filename='/var/log/teleport_restart.log'
+)
+
+def run():
+    try:
+        res = os.popen('ps -C tp_core -o pid,cmd').readlines()
+        if len(res) < 2:
+            logging.warning("teleport need to restart")
+            if os.system("/etc/init.d/teleport restart") == 0: # 0表示命令执行成功，1表示失败
+                logging.info("teleport restart successful")
+    except:
+        logging.error(sys.exc_info()[1])
+
+if __name__ == '__main__':
+    run()
